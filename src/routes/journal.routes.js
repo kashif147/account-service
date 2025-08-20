d2346048f21f6dbf932d719dd6ebb7ed7ae20605
shahnazkashif <shahnaz.azizm@gmail.com>
2025-08-20 16:40:22 +0100

    Authorization

--- src/routes/journal.routes.js
+++ src/routes/journal.routes.js
@@ -5,13 +5,15 @@ import {
 import { invoice, creditNote, receipt, listJournals, claimApplicationCredit, writeOff, changeCategory } from "../controllers/journal.controller.js";
 import router from "./reports.routes.js";
 import validate from "../middlewares/validate.js";
+import verifyJWT from "../middlewares/verifyJWT.js";
 
-router.get("/", listJournalsRules, listJournals);
-router.post("/invoice", invoiceRules, validate, invoice);
-router.post("/receipt", receiptRules, validate, receipt);
-router.post("/credit-note", creditNoteRules, validate, creditNote);
-router.post("/writeoff", writeOffRules, validate, writeOff);
-router.post("/change-category", changeCategoryRules, validate, changeCategory);
-router.post("/claim-credit", claimApplicationCreditRules, validate, claimApplicationCredit)
+router.get("/", verifyJWT, listJournalsRules, listJournals);
+router.post("/invoice",verifyJWT, invoiceRules, validate, invoice);
+router.post("/receipt",verifyJWT, receiptRules, validate, receipt);
+router.post("/credit-note",verifyJWT, creditNoteRules, validate, creditNote);
+router.post("/writeoff",verifyJWT, writeOffRules, validate, writeOff);
+router.post("/change-category",verifyJWT, changeCategoryRules, validate, changeCategory);
+router.post("/claim-credit",verifyJWT, claimApplicationCreditRules, validate, claimApplicationCredit)
+
 
 export default router
