b8344300997afcada25290edff0abb6a198abb2a
dev-sadeeq0023 <sadeeqln8@gmail.com>
2025-08-23 00:36:01 +0500

    added

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 export default {
   testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  moduleFileExtensions: ["js", "json"]
+  roots: ["<rootDir>"],
+  moduleFileExtensions: ["js", "json"],
+  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+  collectCoverageFrom: ["**/*.js", "!**/node_modules/**", "!**/coverage/**", "!jest.config.js", "!server.js"],
 };
+
+// export default {
+//   testEnvironment: "node",
+//   roots: ["<rootDir>/src"],
+//   moduleFileExtensions: ["js", "json"]
+// };
