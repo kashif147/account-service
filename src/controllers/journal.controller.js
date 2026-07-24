c98a392ff881b4edd3d99199ee89342c7751c167
Kashif <kashif147@gmail.com>
2026-07-24 15:55:04 +0100

    journal controller

--- src/controllers/journal.controller.js
+++ src/controllers/journal.controller.js
@@ -19,7 +19,6 @@ import { getMemberTrackedAccountCodes } from "../helpers/coaAccountCodes.helper.
 import { stripeFeeBreakdown } from "../helpers/fees.js";
 import { publishDomainEvent, EVENT_TYPES } from "../rabbitMQ/events.js";
 import { notifyMemberFinanceUpdated } from "../services/memberFinanceRealtimeNotify.service.js";
-import { publishMemberCreditReportingEvent } from "../services/memberCreditReportingPublish.service.js";
 import { notifyMemberPaymentReceiptPosted } from "../services/memberReceiptReminderNotify.service.js";
 import { globalDBLimiter } from "../config/globalLimiter.js";
 import { randomUUID } from "crypto";
@@ -480,13 +479,6 @@ export async function postBalancedJournal({
         memberId,
         docType: txn.docType,
         docNo: txn.docNo,
-      }).catch(() => {});
-      publishMemberCreditReportingEvent({
-        tenantId: resolvedTenantId,
-        memberId,
-        docType: txn.docType,
-        docNo: txn.docNo,
-        correlationId: req.headers?.["x-correlation-id"],
       }).catch(() => {});
     }
 
